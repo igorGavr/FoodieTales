09675c7f68494d694e1658e44ab62be9d0ce1e9f
igorGavr <ragamuffinrogi@gmail.com>
2023-01-31 15:25:57 +0200

    add app.cart

--- config/settings.py
+++ config/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'apps.accounts',
     'apps.blog',
     'apps.shop',
+    'apps.cart',
 ]
 
 
--- config/urls.py
+++ config/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path('', include('apps.blog.urls')),
     path('accounts/', include('apps.accounts.urls')),
     path('shop/', include('apps.shop.urls')),
+    path('cart/', include('apps.cart.urls')),
 ]
 
 
